4d5c85b4d36c533bbcb46e47bd3c4ab8517e4b7e
Federica <federica.rotiroti3@gmail.com>
2026-03-15 17:28:58 +0100

    feat: local fonts

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import '@fontsource/syne/400.css'
+import '@fontsource/syne/600.css'
+import '@fontsource/syne/700.css'
+import '@fontsource/syne/800.css'
+import '@fontsource/dm-mono/300.css'
+import '@fontsource/dm-mono/400.css'
+import '@fontsource/dm-mono/500.css'
+import '@fontsource/dm-sans/300.css'
+import '@fontsource/dm-sans/400.css'
+import '@fontsource/dm-sans/500.css'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
